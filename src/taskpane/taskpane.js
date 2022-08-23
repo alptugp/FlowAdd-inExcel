a3ab215b8e89e64fed54cbc179186a8a79546234
alptugp <alptugati@gmail.com>
2022-08-23 15:11:35 +0100

    Allowed the user to run all the functions in the active worksheet via pressing the Run button in the taskpane

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -15,19 +15,9 @@ Office.onReady(() => {
 export async function run() {
   try {
     await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
+      let activeWorksheet = context.workbook.worksheets.getActiveWorksheet();
+      activeWorksheet.calculate(true);
       await context.sync();
-      console.log(`The range address was ${range.address}.`);
     });
   } catch (error) {
     console.error(error);
